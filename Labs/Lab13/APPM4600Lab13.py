6dbc7a30c3af206bfc1e43aa394b37110d487924
Kayla Zhong <kazh2627@colorado.edu>
2024-04-23 09:59:29 -0600

    code for lab 13

---
+++ Labs/Lab13/APPM4600Lab13.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.linalg as la
+import scipy.linalg as scila
+import time
+
+
+def driver():
+
+     ''' create  matrix for testing different ways of solving a square 
+     linear system'''
+
+     '''' N = size of system'''
+     N = 100
+ 
+     ''' Right hand side'''
+     b = np.random.rand(N,1)
+     A = np.random.rand(N,N)
+
+     start = time.perf_counter_ns()
+     x = scila.solve(A,b)
+     end = time.perf_counter_ns()
+     print(end-start)
+     start = time.perf_counter_ns()
+     x_lu = scila.lu_solve(scila.lu_factor(A), b)
+     end = time.perf_counter_ns()
+     print(end-start)
+     
+     test = np.matmul(A,x)
+     r = la.norm(test-b)
+
+     test_lu = np.matmul(A, x_lu)
+     r_lu = la.norm(test_lu - b)
+     
+     print(r)
+     print(r_lu)
+
+     ''' Create an ill-conditioned rectangular matrix '''
+     N = 10
+     M = 5
+     A = create_rect(N,M)     
+     b = np.random.rand(N,1)
+
+
+     
+def create_rect(N,M):
+     ''' this subroutine creates an ill-conditioned rectangular matrix'''
+     a = np.linspace(1,10,M)
+     d = 10**(-a)
+     
+     D2 = np.zeros((N,M))
+     for j in range(0,M):
+        D2[j,j] = d[j]
+     
+     '''' create matrices needed to manufacture the low rank matrix'''
+     A = np.random.rand(N,N)
+     Q1, R = la.qr(A)
+     test = np.matmul(Q1,R)
+     A =    np.random.rand(M,M)
+     Q2,R = la.qr(A)
+     test = np.matmul(Q2,R)
+     
+     B = np.matmul(Q1,D2)
+     B = np.matmul(B,Q2)
+     return B     
+          
+  
+if __name__ == '__main__':
+      # run the drivers only if this is called from the command line
+      driver()       
